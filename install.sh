bfc544861ed25ec09a7d4575411334ef17d89d90
Eric Wood <eric.wood@datadoghq.com>
2023-08-31 15:21:20 -0600

    aliases

--- install.sh
+++ install.sh
@@ -2,15 +2,18 @@ setup-repo devtools
 
 git config --global user.name "Eric Wood"
 git config --global user.email "eric.wood@datadoghq.com"
+git config --global core.editor "vim"
+git config --global alias.mr "push -u origin HEAD"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cat >> ~/.zshrc <<-EOM
 ZSH_THEME="nicoulaj"
-ZSH_TMUX_AUTOSTART=true
-ZSH_TMUX_ITERM2=true
-plugins=(git tmux)
+plugins=(git)
 
 source $ZSH/oh-my-zsh.sh
+
+alias psqlws='psql postgres://dog:dog@localhost:15433/dogdata'
+
 source ~/.config/dogweb.shellrc
 EOM
