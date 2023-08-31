1e47e44a60ff727450db84b87ed13930ca786d39
Eric Wood <eric.wood@datadoghq.com>
2023-08-31 10:42:38 -0600

    Configur OMZ

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
+setup-repo devtools
+
 git config --global user.name "Eric Wood"
 git config --global user.email "eric.wood@datadoghq.com"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cat >> ~/.zshrc <<-EOM
+ZSH_THEME="nicoulaj"
+plugins=(git tmux)
+
+source $ZSH/oh-my-zsh.sh
+EOM
